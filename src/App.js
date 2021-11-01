d62719dc121ce16cfe24ac6b23197a9abeee8cd5
Javeria Anwer <javeria.anwer@gmail.com>
2021-11-01 20:16:03 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const answer_list= [];   
+
+export function Template(props) {
+  const list =[];
+
+  props.ques.forEach((prop,i)=>{
+    list.push(<div key={i}><div id={prop}></div><div id={props.opt + prop} data-ques-id={prop} key={i}></div></div>);
+  });
+  
+  function onClickSubmit(event){
+    const ans = {
+      quiz_id : props.quiz_id,
+      ques_ans :answer_list
+    };
+    event.preventDeafult();
+  }
+
+  console.log(props.quiz_id);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">{props.title}
+      <form id={props.quiz_id} onSubmit={onClickSubmit}>
+        {list}
+        <input type="submit" id={"btn_"+ props.quiz_id} value="submit" name="submit"/>
+      </form>
     </div>
   );
 }
 
-export default App;
+export function Question(prop) {
+  return (          
+    <p>{prop.title}</p>  
+  );
+}
+
+export function Options(props){
+
+  const property = props.title; 
+  const list = [];
+  
+  property.forEach((prop, i) => {
+    list.push(<div key={i}><input type="radio" value={prop} name={props.id} key={i}/>{prop}</div>);
+  });
+ 
+  function onChangeValue(event) {
+    var q_id = props.id;
+    answer_list.filter(s => s.ques_id.includes(q_id)).length > 0? answer_list.filter(s => s.ques_id.includes(q_id))[0].answer=  event.target.value:answer_list.push({ ques_id: q_id, answer: event.target.value});
+  }
+  
+  return(
+     <div onChange={onChangeValue}>{list}</div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,75 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import * as App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const option_keyword = "opt";
+const ques_list = [];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const quiz_question = [
+    {
+        quiz_id:"1",
+        quiz_title:"General Quiz",
+        questions:[
+            {
+                ques_id:"Q1",
+                question:"How old are you?",
+                options:[5,8,12,14]
+            },
+            
+            {
+                ques_id:"Q2",
+                question:"What are you doing?",
+                options:["nothing","busy","talking to you","playing games"]
+            },
+            
+            {
+                ques_id:"Q3",
+                question:"What's the diameter of the sun?",
+                options:["don't know","let me think about it","wait, let me check internet","lame.."]
+            }
+        ]
+    },
+    {
+        quiz_id:"2",
+        quiz_title:"General Knowledge",
+        questions:[
+            {
+                ques_id:"Q1",
+                question:"Is your father a scientist?",
+                options:["Yes", "No"]
+            },
+            
+            {
+                ques_id:"Q2",
+                question:"Is your Mother an engineer?",
+                options:["Yes", "No"]
+            },
+            
+            {
+                ques_id:"Q3",
+                question:"Will you encourage your childern to become a doctor?",
+                options:["Yes", "No"]
+            }
+        ]
+    }
+]
+
+
+quiz_question.forEach((qus,i)=>{
+    if(qus.quiz_id==2)
+    {
+        qus.questions.forEach((q_id,i)=>{
+            ques_list.push(q_id.ques_id);
+        });        
+
+        ReactDOM.render(<App.Template title={qus.quiz_title} quiz_id={qus.quiz_id} opt={option_keyword} ques={ques_list}/>, document.getElementById('root'));
+        qus.questions.forEach((ques,i)=>{
+            ReactDOM.render(<App.Question title={ques.question} />, document.getElementById(ques.ques_id));
+            ReactDOM.render(<App.Options title={ques.options} id={ques.ques_id}  />, document.getElementById(option_keyword + ques.ques_id));
+        });
+    }
+});
+
 reportWebVitals();
